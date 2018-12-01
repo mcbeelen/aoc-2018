b6bb9b359c2ac370925087e78444f99a2cc26b9c
Marco Beelen <marco.beelen@ximedes.com>
2018-12-01 09:37:02 +0100

    Solved part TWO of day ONE with a CircularBuffer as a Sequence

---
+++ src/main/kotlin/day01/SameFrequenceFinder.kt
@@ -0,0 +1,56 @@
+package day01
+
+import kotlin.system.measureTimeMillis
+
+class SameFrequenceFinder {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val modulations = DAY_1_INPUT.trimIndent().lines().map { it.toInt() }.toList()
+
+            val timeElapsed = measureTimeMillis {
+                println(findFirstRecurringFrequency(modulations))
+
+            }
+            print("Solved it in: ${timeElapsed}ms")
+
+        }
+    }
+}
+
+
+
+fun findFirstRecurringFrequency(input: List<Int>): Int {
+
+    var currentFrequency = 0
+    val frequencySet : MutableSet<Int> = HashSet()
+
+    circularSequence(input)
+            .takeWhile { !frequencySet.contains(currentFrequency) }
+            .forEach {
+                frequencySet.add(currentFrequency)
+                currentFrequency += it
+            }
+
+    return currentFrequency
+
+}
+
+private fun circularSequence(input: List<Int>): Sequence<Int> {
+    return sequence {
+        var index = 0
+
+        while (true) {
+            yield(input[index])
+            index++
+            if (index == input.size) {
+                index = 0
+            }
+        }
+
+    }
+}
+
+
+
