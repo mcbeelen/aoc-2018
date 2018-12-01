064aeceaaebb8f0b009d576285639ba29da93ad4
Marco Beelen <marco.beelen@ximedes.com>
2018-12-01 09:52:00 +0100

    Added measuring the elapsed time

--- src/main/kotlin/day01/SameFrequenceFinder.kt
+++ src/main/kotlin/day01/SameFrequenceFinder.kt
@@ -7,24 +7,25 @@ class SameFrequenceFinder {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
+
             val modulations = DAY_1_INPUT.trimIndent().lines().map { it.toInt() }.toList()
 
             val timeElapsed = measureTimeMillis {
-                println(findFirstRecurringFrequency(modulations))
+                println("First frequence seen twice: " + findFirstRecurringFrequency(modulations))
 
             }
-            print("Solved it in: ${timeElapsed}ms")
+            println("  Solved it in: ${timeElapsed}ms")
+
 
         }
     }
 }
 
 
-
 fun findFirstRecurringFrequency(input: List<Int>): Int {
 
     var currentFrequency = 0
-    val frequencySet : MutableSet<Int> = HashSet()
+    val frequencySet: MutableSet<Int> = HashSet()
 
     circularSequence(input)
             .takeWhile { !frequencySet.contains(currentFrequency) }
