dc67399a4836368b8b3be570ffe4ee565f1d3033
Marco Beelen <marco.beelen@ximedes.com>
2018-12-10 11:21:07 +0100

    Day10: Implemented moving all stars across the sky

--- src/main/kotlin/day10_stars_align/Sky.kt
+++ src/main/kotlin/day10_stars_align/Sky.kt
@@ -4,5 +4,7 @@ class Sky(private val stars: List<Star>) {
 
     fun hasStarAt(position: Position)= stars.any { it.isAt(position) }
 
+    fun moveStars(): Sky = Sky(stars.map { it.move() })
+
 }
 
--- src/test/kotlin/day10_stars_align/SkyTest.kt
+++ src/test/kotlin/day10_stars_align/SkyTest.kt
@@ -8,19 +8,50 @@ import org.junit.Test
 class SkyTest {
 
 
+    private val originalSky: Sky
+        get() {
+            val sky = Sky(listOf(
+                    parseInputToStar("position=<3, 9> velocity=<1, -2>"),
+                    parseInputToStar("position=< 9,  1> velocity=< 0,  2>")
+            ))
+            return sky
+        }
+
     @Test
     fun itShouldBeAbleToLocateStars() {
 
-        val sky = Sky(listOf(
-                parseInputToStar("position=<3, 9> velocity=<1, -2>"),
-                parseInputToStar("position=< 9,  1> velocity=< 0,  2>")
-        ))
+        val sky = originalSky
 
         assertTrue(sky.hasStarAt(Position(3, 9)))
         assertTrue(sky.hasStarAt(Position(9, 1)))
 
         assertFalse(sky.hasStarAt(Position(9, 2)))
         assertFalse(sky.hasStarAt(Position(8, 1)))
+
+    }
+
+    @Test
+    fun itShouldBeAbleToMoveAllStarsInTheSky() {
+
+        var sky = originalSky
+
+        sky = sky.moveStars()
+
+        assertFalse(sky.hasStarAt(Position(3, 9)))
+        assertFalse(sky.hasStarAt(Position(9, 1)))
+
+        assertTrue(sky.hasStarAt(Position(4, 7)))
+        assertTrue(sky.hasStarAt(Position(9, 3)))
+
+
+        sky = sky.moveStars()
+
+        assertFalse(sky.hasStarAt(Position(4, 7)))
+        assertFalse(sky.hasStarAt(Position(9, 3)))
+
+        assertTrue(sky.hasStarAt(Position(5, 5)))
+        assertTrue(sky.hasStarAt(Position(9, 5)))
+
 
     }
 }
