b75562eb893f79c50d84119316b7bd6150776153
Marco Beelen <marco.beelen@ximedes.com>
2018-12-10 11:16:16 +0100

    Day10: Additional starLocation tests

--- src/test/kotlin/day10_stars_align/SkyTest.kt
+++ src/test/kotlin/day10_stars_align/SkyTest.kt
@@ -1,5 +1,6 @@
 package day10_stars_align
 
+import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
@@ -10,9 +11,16 @@ class SkyTest {
     @Test
     fun itShouldBeAbleToLocateStars() {
 
-        val sky = Sky(listOf(parseInputToStar("position=<3, 9> velocity=<1, -2>")))
+        val sky = Sky(listOf(
+                parseInputToStar("position=<3, 9> velocity=<1, -2>"),
+                parseInputToStar("position=< 9,  1> velocity=< 0,  2>")
+        ))
 
         assertTrue(sky.hasStarAt(Position(3, 9)))
+        assertTrue(sky.hasStarAt(Position(9, 1)))
+
+        assertFalse(sky.hasStarAt(Position(9, 2)))
+        assertFalse(sky.hasStarAt(Position(8, 1)))
 
     }
 }
