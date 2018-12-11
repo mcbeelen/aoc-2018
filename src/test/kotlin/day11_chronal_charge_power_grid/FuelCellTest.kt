6b12096de8c1cff449d33f317ec9d3feeebce215
Marco Beelen <marco.beelen@ximedes.com>
2018-12-11 08:24:31 +0100

    Extracted FuelCell add make powerLevel a property (to reduce recalculating it multiple times)

--- src/test/kotlin/day11_chronal_charge_power_grid/FuelCellTest.kt
+++ src/test/kotlin/day11_chronal_charge_power_grid/FuelCellTest.kt
@@ -10,32 +10,14 @@ class FuelCellTest {
     @Test
     fun itShouldCalculateThePowerLevel() {
 
-        assertThat(FuelCell(3, 5).calculatePowerLevelInGrid(8), equalTo(4))
+        assertThat(FuelCell(8, 3, 5).powerLevel, equalTo(4))
 
 
-        assertThat(FuelCell(122,79).calculatePowerLevelInGrid(57), equalTo(-5))
-        assertThat(FuelCell(217,196).calculatePowerLevelInGrid(39), equalTo(0))
-        assertThat(FuelCell(101,153).calculatePowerLevelInGrid(71), equalTo(4))
+        assertThat(FuelCell(57, 122,79).powerLevel, equalTo(-5))
+        assertThat(FuelCell(39, 217,196).powerLevel, equalTo(0))
+        assertThat(FuelCell(71, 101,153).powerLevel, equalTo(4))
 
 
     }
 }
 
-class FuelCell(val x: Int, val y: Int) {
-    fun calculatePowerLevelInGrid(gridSerialNumber: Int): Int {
-        val rackID = x + 10
-        val baseForPowerLevel = ((rackID * y) + gridSerialNumber) * rackID
-        val hundredsDigit = if (baseForPowerLevel < 100) {
-             0 - 5
-        } else {
-            baseForPowerLevel.rem(1000).div(100)
-        }
-        return hundredsDigit - 5
-    }
-
-    override fun toString(): String {
-        return "FuelCell: '${x},${y})"
-    }
-
-
-}
