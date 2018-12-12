b35525c85a34f63593e32775f26d82d73ac8a80c
Marco Beelen <marco.beelen@ximedes.com>
2018-12-12 10:20:17 +0100

    Day 12: Got a  solution passing the example

--- src/main/kotlin/day12_subterranean_sustainability/PlantGrowthTracker.kt
+++ src/main/kotlin/day12_subterranean_sustainability/PlantGrowthTracker.kt
@@ -1,24 +1,82 @@
 package day12_subterranean_sustainability
 
-class PlantGrowthTracker(private val potsWithPlants: Set<Int>, private val transformationsInput: String) {
+data class PlantGrowthTracker(private val potsWithPlants: Set<Int>, private val transformation: Set<Transformation>) {
 
-    constructor(initialState: String, transformationsInput: String) : this(parseInitialState(initialState), transformationsInput)
+    constructor(initialState: String, transformationsInput: String) : this(parseInitialState(initialState), parseTransformations(transformationsInput))
+
+
 
 
     fun sumOfAllPotsContainingPlants(): Int {
-        return 325
+        return potsWithPlants.sum()
     }
 
 
     fun nextGeneration(numberOfGenerations: Int): PlantGrowthTracker {
-        return this
+        if (numberOfGenerations == 0) {
+            return this
+        }
+        return generateNextGeneration().nextGeneration(numberOfGenerations - 1)
+    }
+
+    fun generateNextGeneration(): PlantGrowthTracker {
+        println(this.toString())
+        val nextSetOfPotsWithPlants = (potsWithPlants.min()!! - 2..potsWithPlants.max()!! + 2)
+                .filter { potIndex ->
+                    transformation.any { transformation -> transformation.isApplicable(potIndex, potsWithPlants) }
+                }
+                .toSet()
+        return this.copy(potsWithPlants = nextSetOfPotsWithPlants)
     }
 
 
+    override fun toString(): String {
+        return (0 .. 30)
+                .map{ if (potsWithPlants.contains(it)) { '#' } else { '.' }}
+                .joinToString("")
+    }
 }
 
 internal fun parseInitialState(initialState: String) =
-    initialState.withIndex()
-            .filter { it.value == '#' }
-            .map { it.index }
+        initialState.withIndex()
+                .filter { it.value == '#' }
+                .map { it.index }
+                .toSet()
+
+
+
+
+data class Transformation(val requiredOccupiedPots: Set<Int>, val forbiddenPots: Set<Int>) {
+    fun isApplicable(potIndex: Int, potsWithPlants: Set<Int>): Boolean {
+
+        return requiredOccupiedPots.all { potsWithPlants.contains(potIndex + it) } &&
+                forbiddenPots.none { potsWithPlants.contains(potIndex + it) }
+
+
+    }
+}
+
+
+fun parseTransformations(transformationsInput: String) = transformationsInput.trimIndent().lines()
+        .filter { it.last() == '#' }
+        .map { parseToTransformation(it) }
+        .toSet()
+
+fun parseToTransformation(transformationInput: String) = Transformation (findRequiredOccupiedPots(transformationInput), findForbiddenPots(transformationInput))
+
+fun findForbiddenPots(transformationInput: String): Set<Int> {
+    return findOffsets(transformationInput, '.')
+}
+
+private fun findRequiredOccupiedPots(transformationInput: String): Set<Int> {
+    return findOffsets(transformationInput, '#')
+}
+
+private fun findOffsets(transformationInput: String, c: Char): Set<Int> {
+    return transformationInput.substring(0..5)
+            .withIndex()
+            .filter { it.value == c }
+            .map { it.index - 2}
             .toSet()
+}
+
--- src/test/kotlin/day12_subterranean_sustainability/PlantGrowthTrackerTest.kt
+++ src/test/kotlin/day12_subterranean_sustainability/PlantGrowthTrackerTest.kt
@@ -3,6 +3,7 @@ package day12_subterranean_sustainability
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import com.natpryce.hamkrest.hasSize
+import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Ignore
 import org.junit.Test
@@ -35,6 +36,19 @@ class PlantGrowthTrackerTest {
             assertTrue(it.containsAll(listOf(0, 3, 5, 8, 9, 16, 17, 18, 22, 23, 24)))
         }
     }
+
+
+
+
+    @Test
+    fun isApplicable() {
+        val transformation = Transformation(setOf(-1, 1), setOf(-2, 0, 2))
+
+        val potsWithPlants = parseInitialState(exampleInitialState)
+        assertFalse(transformation.isApplicable(0, potsWithPlants))
+        assertTrue(transformation.isApplicable(4, potsWithPlants))
+    }
+
 
 
     @Test
