5b69b3ea3d4e74bc2ee642bcd4a5b60bca4733b4
Marco Beelen <marco.beelen@ximedes.com>
2018-12-16 21:22:55 +0100

    Switched to using 'listOfCombatants for plotter'

--- src/main/kotlin/day15_beverage_bandits/BattlefieldPlotter.kt
+++ src/main/kotlin/day15_beverage_bandits/BattlefieldPlotter.kt
@@ -12,15 +12,15 @@ fun plot(situation: GoblinVsElvesSimulator) {
     for (y in 0..size) {
         for (x in 0..size) {
             when {
-                isOccupied(x, y, situation.combatants) -> printOccupant(x, y, situation.combatants)
+                isOccupied(x, y, situation.listCombatantsThisRound) -> printOccupant(x, y, situation.listCombatantsThisRound)
                 situation.openSpaces.contains(ScreenCoordinate(x, y)) -> print('.')
                 else -> print('#')
             }
         }
 
         for (x in 0..size) {
-            if (isOccupied(x, y, situation.combatants)) {
-                printHealthInfo(x, y, situation.combatants)
+            if (isOccupied(x, y, situation.listCombatantsThisRound)) {
+                printHealthInfo(x, y, situation.listCombatantsThisRound)
             }
         }
 
@@ -34,16 +34,16 @@ fun plot(situation: GoblinVsElvesSimulator) {
 
 }
 
-private fun printHealthInfo(x: Int, y: Int, combatants: Set<Combatant>) {
+private fun printHealthInfo(x: Int, y: Int, combatants: Collection<Combatant>) {
     val combatant = findCombatantAt(combatants, x, y)
     print(message = " ${combatant.type.name[0]}(${combatant.hitPoints})")
 }
 
-private fun printOccupant(x: Int, y: Int, combatants: Set<Combatant>) {
+private fun printOccupant(x: Int, y: Int, combatants: Collection<Combatant>) {
     print(findCombatantAt(combatants, x, y).type.name[0])
 }
 
-private fun findCombatantAt(combatants: Set<Combatant>, x: Int, y: Int) =
+private fun findCombatantAt(combatants: Collection<Combatant>, x: Int, y: Int) =
         combatants.single { it.position.isAt(x, y) }
 
-private fun isOccupied(x: Int, y: Int, combatants: Set<Combatant>) = combatants.any { it.position.isAt(x, y) }
+private fun isOccupied(x: Int, y: Int, combatants: Collection<Combatant>) = combatants.any { it.position.isAt(x, y) }
