f6284bb91ced833df7ad20918f902dc55093b64e
Marco Beelen <marco.beelen@ximedes.com>
2018-12-16 21:27:25 +0100

    Day 15: Removed dependency on actual combatants in test for InBattleOrder-Comparator

--- src/test/kotlin/day15_beverage_bandits/CombatantInBattleOrderTest.kt
+++ src/test/kotlin/day15_beverage_bandits/CombatantInBattleOrderTest.kt
@@ -5,6 +5,7 @@ import com.natpryce.hamkrest.equalTo
 import day15_beverage_bandits.CreatureType.ELF
 import day15_beverage_bandits.CreatureType.GOBLIN
 import org.junit.Test
+import util.grid.ScreenCoordinate
 import util.grid.isAt
 
 class CombatantInBattleOrderTest {
@@ -12,8 +13,15 @@ class CombatantInBattleOrderTest {
     @Test
     fun compare() {
 
-        val sampleBattlefield = parseIntoBattleField(ENTIRE_SAMPLE_COMBAT)
-        val combatants: List<Combatant> = sampleBattlefield.combatants.sortedWith(combatantInBattleOrder)
+        val combatantsSource = listOf(
+                Combatant(ELF, ScreenCoordinate(3, 3)),
+                Combatant(GOBLIN, ScreenCoordinate(2, 1)),
+                Combatant(ELF, ScreenCoordinate(5, 4)),
+                Combatant(ELF, ScreenCoordinate(1, 4))
+
+
+        )
+        val combatants: List<Combatant> = combatantsSource.sortedWith(combatantInBattleOrder)
 
         combatants.first().let {
             assertThat(it.type, equalTo(GOBLIN))
