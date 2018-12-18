d9e5575bdfb7764959acdc97b309da3d7f68d607
Marco Beelen <marco.beelen@ximedes.com>
2018-12-18 21:56:31 +0100

    Solved day 18: Part two with offset comparision and manual lookup in printed listing

--- src/main/kotlin/day18_lumber_collection/LumberCollectionAreaInput.kt
+++ src/main/kotlin/day18_lumber_collection/LumberCollectionAreaInput.kt
@@ -1,5 +1,31 @@
 package day18_lumber_collection
 
+import day18_lumber_collection.Acre.*
+import util.grid.ScreenCoordinate
+
+fun parseLumberCollectionArea(scanOfTheArea: String): LumberCollectionArea {
+
+    val areaMap: MutableMap<ScreenCoordinate, Acre> = HashMap()
+
+    scanOfTheArea.trimIndent().lines().withIndex()
+            .forEach { line ->
+                line.value.withIndex().forEach { acre ->
+                    areaMap[ScreenCoordinate(acre.index, line.index)] = acreFrom(acre.value)
+                }
+            }
+
+
+    return LumberCollectionArea(areaMap)
+
+}
+
+fun acreFrom(char: Char) = when (char) {
+    '|' -> TREES
+    '#' -> LUMBERYARD
+    else -> OPEN
+}
+
+
 const val LUMBER_COLLECTION_AREA_INPUT = """
 |.....#.....||...#.|###..#..|#...........|...|..|#
 ||..|.......|.|.|..|.|....#.|#..|..|.|#.....|.|..|
---
+++ src/main/kotlin/day18_lumber_collection/LumberYardSolver.kt
@@ -0,0 +1,32 @@
+package day18_lumber_collection
+
+class LumberYardSolver {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+
+            var area = parseLumberCollectionArea(LUMBER_COLLECTION_AREA_INPUT)
+            for (minute in 1 .. 1_000) {
+                area = letMinutePass(area)
+                println("After ${minute} minutes: ${area.countOfTrees() * area.countOfLumberyards()}")
+            }
+
+
+
+        }
+    }
+}
+
+class OffsetCalculator {
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+
+            println("1_000_000_000 has offset ${1_000_000_000.rem(28)}")
+            println("972 also has an offset ${972.rem(28)}")
+
+        }
+    }
+}
