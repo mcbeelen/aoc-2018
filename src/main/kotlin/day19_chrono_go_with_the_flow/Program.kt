121739c6a4a0a8e04be0ef855b345324a9e84802
Marco Beelen <marco.beelen@ximedes.com>
2018-12-21 16:14:58 +0100

    Adjusted the ElfCode Executable support breakpoints and debuggers, which allows Day21 ProblemSolvers to hook into the executionFlow to inspect register[4] on instruction 28

--- src/main/kotlin/day19_chrono_go_with_the_flow/Program.kt
+++ src/main/kotlin/day19_chrono_go_with_the_flow/Program.kt
@@ -6,50 +6,6 @@ import kotlin.system.measureTimeMillis
 
 data class Program(val boundRegister: Int, val instructions: List<Instruction>)
 
-class Executable(val program: Program) {
 
-    var registers = intArrayOf(0, 0, 0, 0, 0, 0)
-
-    var instuctionPointer : Int = 0
-
-    private fun execute() {
-        while (instuctionPointer in 0 .. program.instructions.size - 1) {
-            executeInstruction()
-        }
-    }
-
-    private fun executeInstruction() {
-
-        registers[program.boundRegister] = instuctionPointer
-        registers = performOperation(program.instructions[instuctionPointer], registers)
-        instuctionPointer = registers[program.boundRegister]
-
-        instuctionPointer++
-
-    }
-
-
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-
-            val time = measureTimeMillis {
-                val program = parseProgram(ACTUAL_INSTRUCTIONS) //EXAMPLE_INSTRUCTIONS
-                val execution = Executable(program)
-
-                execution.execute()
-
-                println(execution.registers[0])
-
-            }
-
-            println("Solved it in ${time}ms")
-
-        }
-    }
-
-
-
-}
 
 
