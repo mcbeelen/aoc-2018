dd3bc808ccfbdc036c0d4020a0aa934b9be1388e
Marco Beelen <mcbeelen@gmail.com>
2019-12-01 08:44:52 +0100

    Extracted the fuelRequired-method for production

---
+++ src/main/kotlin/y2019/day01_gonogo_spacecraft/FuelRequirementCalculator.kt
@@ -0,0 +1,7 @@
+package y2019.day01_gonogo_spacecraft
+
+
+fun fuelRequired(mass: Int): Int {
+    return mass / 3 - 2
+
+}
--- src/test/kotlin/y2019/day01_gonogo_spacecraft/FuelRequirementCalculatorTest.kt
+++ src/test/kotlin/y2019/day01_gonogo_spacecraft/FuelRequirementCalculatorTest.kt
@@ -8,16 +8,11 @@ class FuelRequirementCalculatorTest {
 
     @Test
     fun `examples from the instructions`() {
-
         assertThat(fuelRequired(12), equalTo(2))
         assertThat(fuelRequired(14), equalTo(2))
         assertThat(fuelRequired(1969), equalTo(654))
         assertThat(fuelRequired(100756), equalTo(33583))
     }
 
-    private fun fuelRequired(mass: Int): Int {
-        return mass / 3 - 2
-
-    }
 }
 
