e48d7887d965212c42f1525e4ef380db39d3f4ae
Marco Beelen <marco@mcbeelen.net>
2019-12-02 09:42:59 +0100

    Implemented OpCode 2 and 99

--- src/main/kotlin/y2019/day02_int_code_program/IntCodeSimulator.kt
+++ src/main/kotlin/y2019/day02_int_code_program/IntCodeSimulator.kt
@@ -4,23 +4,38 @@ data class IntCodeSimulator(val intCode: List<Int>, val cursor : Int = 0) {
 
 
     constructor(intCode: String) : this(parseIntCode(intCode))
+    constructor(intCode: String, cursor: Int) : this(parseIntCode(intCode), cursor)
 
 
     fun tick(): IntCodeSimulator {
         val newIntCode = intCode.toMutableList()
         val firstInputPosition = intCode[cursor + 1]
         val secondInputPosition = intCode[cursor + 2]
-        val i = intCode[firstInputPosition] + intCode[secondInputPosition]
+
+        val output =  when (intCode[cursor]) {
+            1 -> intCode[firstInputPosition] + intCode[secondInputPosition]
+            2 -> intCode[firstInputPosition] * intCode[secondInputPosition]
+            else -> 0
+        }
+
         val resultPosition = intCode[cursor + 3]
-        newIntCode[resultPosition] = i
-        return IntCodeSimulator(newIntCode)
+        newIntCode[resultPosition] = output
+        return IntCodeSimulator(newIntCode, cursor + 4)
     }
+
+    fun isProgramFinished() = intCode[cursor] == 99
 }
 
 
 fun parseIntCode(intCode: String) = intCode.split(',').map { it.toInt() }
 
 
+fun main() {
+    val intCode = parseIntCode(INPUT).toMutableList()
+    intCode[1] = 12
+    intCode[2] = 2
+
+}
 
 
 private const val INPUT = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,13,1,19,1,19,10,23,1,23,13,27,1,6,27,31,1,9,31,35,2,10,35,39,1,39,6,43,1,6,43,47,2,13,47,51,1,51,6,55,2,6,55,59,2,59,6,63,2,63,13,67,1,5,67,71,2,9,71,75,1,5,75,79,1,5,79,83,1,83,6,87,1,87,6,91,1,91,5,95,2,10,95,99,1,5,99,103,1,10,103,107,1,107,9,111,2,111,10,115,1,115,9,119,1,13,119,123,1,123,9,127,1,5,127,131,2,13,131,135,1,9,135,139,1,2,139,143,1,13,143,0,99,2,0,14,0"
--- src/test/kotlin/y2019/day02_int_code_program/IntCodeSimulatorTest.kt
+++ src/test/kotlin/y2019/day02_int_code_program/IntCodeSimulatorTest.kt
@@ -2,6 +2,8 @@ package y2019.day02_int_code_program
 
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class IntCodeSimulatorTest {
@@ -14,6 +16,28 @@ class IntCodeSimulatorTest {
         simulator = simulator.tick()
 
         assertThat(simulator.intCode[3], equalTo(70))
+        assertThat(simulator.cursor, equalTo(4))
+    }
+
+    @Test
+    fun `simpulateOpsCode2 multiply`() {
+        val intCode = "1,9,10,70,2,3,11,0,99,30,40,50"
+        var simulator = IntCodeSimulator(intCode, 4)
+
+        simulator = simulator.tick()
+
+        assertThat(simulator.intCode[0], equalTo(3500))
+        assertThat(simulator.cursor, equalTo(8))
+    }
+
+
+    @Test
+    fun `cursor points to 99 means program is finished`() {
+        val intCode = "1,9,10,70,2,3,11,0,99,30,40,50"
+
+        assertFalse(IntCodeSimulator(intCode, 0).isProgramFinished())
+        assertFalse(IntCodeSimulator(intCode, 4).isProgramFinished())
+        assertTrue(IntCodeSimulator(intCode, 8).isProgramFinished())
 
     }
 }
