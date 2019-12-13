ca755e5bbe95c60d4687f99f66aac40a84d33dd9
Marco Beelen <marco@mcbeelen.net>
2019-12-13 09:57:27 +0100

    Solved y2019d13p1

--- src/main/kotlin/y2019/computer/Screen.kt
+++ src/main/kotlin/y2019/computer/Screen.kt
@@ -1,4 +1,15 @@
 package y2019.computer
 
+import util.grid.ScreenCoordinate
+
 class Screen {
+
+    private val buffer: MutableMap<ScreenCoordinate, Char> = HashMap()
+
+    fun paint(coordinate: ScreenCoordinate, char: Char) {
+        buffer[coordinate] = char
+    }
+
+    fun calculateNumberOfTiles(char: Char): Int = buffer.values.count { it == char }
+
 }
--- src/main/kotlin/y2019/day016_pong/ArcadeCabinet.kt
+++
@@ -1,4 +0,0 @@
-package y2019.day016_pong
-
-class ArcadeCabinet {
-}
